f474ca8dfad9dbc42fd0c12a296b717c7f351936
Priyanshu Singh <2020ugmm076@nitjsr.ac.in>
2023-07-18 18:56:21 +0530

    Deployment Fix v4

--- server/index.js
+++ server/index.js
@@ -14,16 +14,19 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://online-judge-client.onrender.com/"],
-    methods: "GET, POST",
-    allowedHeaders: "Content-Type, Authorization",
+    origin: [
+      "http://localhost:3000",
+      "https://online-judge-client.onrender.com",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
 app.use(require("./router/auth"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
