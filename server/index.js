784d032bc8d908269f7687924860da33b355a874
Priyanshu Singh <2020ugmm076@nitjsr.ac.in>
2023-07-18 16:49:30 +0530

    Deployment Fix v1

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://online-judge-client.onrender.com/"],
     methods: "GET, POST",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
