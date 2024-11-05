7340bf60f036e8a271f2f476449486166a556a51
Priyanshu Singh <therealgrindin.001@gmail.com>
2024-11-06 01:46:58 +0530

    Bug Fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://oj.priyanshudevsingh.me"],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     credentials: true,
   })
